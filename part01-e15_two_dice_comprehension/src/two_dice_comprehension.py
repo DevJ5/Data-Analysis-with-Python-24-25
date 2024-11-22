d880d41b84b0ccf6d40e4577bb79e29bb2424744
DevJ5 <idevelopjs@gmail.com>
2024-11-22 15:18:46 +0100

    Exercise 11 - 15 done

--- part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
+++ part01-e15_two_dice_comprehension/src/two_dice_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+
 def main():
-    pass
+    result = [(x, y) for x in range(1, 7) for y in range(1, 7) if x + y == 5]
+    for pair in result:
+        print(pair)
+
 
 if __name__ == "__main__":
     main()
