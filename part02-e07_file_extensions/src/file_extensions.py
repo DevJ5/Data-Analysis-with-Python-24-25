3ed86f87eb2779958f988bccda13e97d84602766
DevJ5 <idevelopjs@gmail.com>
2024-11-28 17:13:58 +0100

    Part 02 Exercise 06 & 07 done

--- part02-e07_file_extensions/src/file_extensions.py
+++ part02-e07_file_extensions/src/file_extensions.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python3
+#!/usr/bin/env python3
+
 
 def file_extensions(filename):
-    return ([], {})
+    filenames_no_extension = []
+    filenames_and_extensions = {}
+
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            line = line.strip()
+            if len(line) == 0:
+                continue
+            elif "." not in line:
+                filenames_no_extension.append(line)
+            else:
+                extension = line.split(".")[-1]
+                try:
+                    filenames_and_extensions[extension].append(line)
+                except:
+                    filenames_and_extensions[extension] = [line]
+
+    return (filenames_no_extension, filenames_and_extensions)
+
 
 def main():
-    pass
+    filenames_no_extension, filenames_and_extensions = file_extensions(
+        "src/filenames.txt"
+    )
+    print(f"{len(filenames_no_extension)} files with no extension")
+    for k, v in filenames_and_extensions.items():
+        print(f"{k} {len(v)}")
+
 
 if __name__ == "__main__":
     main()
